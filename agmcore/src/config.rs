85925c4f5f624cbb58b62ded0f1019927b31fb34
ilicid <lucaslauritsen29@gmail.com>
2026-01-15 14:36:28 +0100

    fixed syntax error

--- agmcore/src/config.rs
+++ agmcore/src/config.rs
@@ -9,22 +9,19 @@ pub fn ensure_config_file() -> io::Result<()> {
         if !program_dir.exists() {
             fs::create_dir_all(&program_dir)?;
             println!("Created program directory: {}", program_dir.display());
-        }
-        else {
+        } else {
             println!("Program directory already exists: {}", program_dir.display());
         }
 
         let config_file = program_dir.join("config.yaml");
-        
+
         if !config_file.exists() {
             fs::File::create(&config_file)?;
             println!("Created config file: {}", config_file.display());
-        }
-        
-        else {
+        } else {
             println!("Config file already exists: {}", config_file.display());
         }
+    }
 
     Ok(())
 }
-
